b5fad77e27ab69e7639d55d7e17c2ab337d681f0
Alex Griffen <radhack242@gmail.com>
2019-08-01 10:40:44 -0700

    update screener.config.js with states and sauce

--- screener.config.js
+++ screener.config.js
@@ -1,12 +1,20 @@
+var Steps = require('screener-runner/src/steps');
 module.exports = {
   // full repository name for your project:
-  projectRepo: 'alexgriffen/sauceVisualRunner',
+  projectRepo: 'alexgriffen/allTheTests',
 
   // this example assumes Environment Variables listed below exist on your system:
   apiKey: process.env.SCREENER_API_KEY,
-
+  // ignore: '.dxGwqD',
   // array of UI states to capture visual snapshots of.
   // each state consists of a url and a name.
+  sauce: {
+    username: process.env.SAUCE_USERNAME,
+    accessKey: process.env.SAUCE_ACCESS_KEY,
+    maxConcurrent: 100, // optional available concurrency you have from Sauce Labs
+    // extendedDebugging: true, // optional
+    tunnelIdentifier: 'USWEST' // optional
+  },
   states: [
     {
       url: 'http://saucelabs.com',
@@ -19,6 +27,24 @@ module.exports = {
     {
       url: 'https://www.google.com/search?hl=en&authuser=0&tbm=isch&source=hp&biw=1440&bih=765&ei=T5wSXYH8GZ-w0PEP5K2j8A8&q=cute+puppies&oq=cute+puppies&gs_l=img.3..0l10.3194.5428..5562...1.0..0.67.813.14......0....1..gws-wiz-img.....0..0i10i24.8j1dZf6A_Qs',
       name: 'Cute Puppies'
+    }
+  ],
+  browsers: [
+    {
+      browserName: 'chrome',
+      version: '75'
+    },
+    {
+      browserName: 'microsoftedge',
+      version: '17.17134'
+    },
+    {
+      browserName: 'firefox',
+      version: '68'
+    },
+    {
+      browserName: 'internet explorer',
+      version: '11'
     }
   ]
 };
